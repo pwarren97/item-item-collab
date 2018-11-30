7be3730be27b8534b8314f43149171b29cf95b02
Peyton Warren <pwarren97@gmail.com>
2018-11-30 17:01:16 -0600

    Now has a mean for the items worked out, the similarity function is in progress

---
+++ main.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+import pandas as pd
+import numpy as np
+import math
+
+data_full = pd.read_csv("data/data-full.csv", names=["MovieID", "UserID", "Rating"])
+movie_names = pd.read_csv("data/movie_names.csv", error_bad_lines=False, names=["MovieID", "YearOfRelease", "Title"])
+
+# creates a new table out of data_full with the means of the ratings in place of Rating
+Mean = data_full.groupby(["MovieID"], as_index=False, sort=False).mean().rename(columns = {'Rating': 'Mean_Rating'})
+Mean = Mean.drop('UserID', 1)
+
+# create a ratings table that includes the mean of the ratings in a column
+Ratings = pd.merge(data_full, Mean, on='MovieID', how='left', sort=False)
+# print Ratings
+
+
+movie_data_all_append = pd.DataFrame()
+
+user_data = Ratings[Ratings['UserID'] != 320]
+distinct_movies = np.unique(suer_data['MovieID'])
+i = 1
+for movie in distinct_movies:
+    if i%10 == 0:
+
+    print i, "out of ", len(distinct_movies)
+
+    movie_data_all=pd.DataFrame()
+
+    movie_data = Ratings[Ratings['MovieID']==movie]
+
+    movie_data = movida_data[]
